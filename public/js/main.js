6d7dbf356272d629afe3671fc50a4e953570b3e4
Joan <jdomenechivars@gmail.com>
2017-08-28 12:19:53 +0200

    start implement single page htlm

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,124 @@
 //MAKE APEAR THE SETTINGS PANEL//
 
 $(document).ready(function () {
+
   $(".sett").click(function () {
-    $(".settingsPanelShadow").css("display", "block");
+    $(".settingsPanelShadow").show();
   });
-});
 
-$(document).ready(function () {
+
   $(".settingsPanelShadow").click(function () {
-    $(".settingsPanelShadow").css("display", "none");
+    $(".settingsPanelShadow").hide();
   });
-});
 
 
-$(document).ready(function () {
-  $(".menuButtons").click(function () {
-    $(this).css("display", "none");
+  $(".bigButtons").click(function () {
+
+    $(".aback").show();
+    $(".menu1").hide();
+    $(".menu2").show();
 
   });
+
+  $(".littleMenuButtons").click(function () {
+
+    $(".aback").show();
+
+  });
+
+  $(".news").click(function () {
+    
+    $("#title").empty();
+    $("#title").append("NEWS & EVENTS");
+
+    $("#mainBackground").hide();
+    $("#news").show();
+    $("#schedule").hide();
+    $("#scores").hide();
+    $("#teams").hide();
+    $("#chat").hide();
+    $('#contact').hide();
+
+  });
+
+  $(".schedule").click(function () {
+    
+    $("#title").empty();
+    $("#title").append("SCHEDULE");
+
+    $("#mainBackground").hide();
+    $("#news").hide();
+    $("#schedule").show();
+    $("#scores").hide();
+    $("#teams").hide();
+    $("#chat").hide();
+    $('#contact').hide();
+
+  });
+  
+    $(".scores").click(function () {
+      
+      
+    $("#title").empty();
+    $("#title").append("SCORES");
+
+
+    $("#mainBackground").hide();
+    $("#news").hide();
+    $("#schedule").hide();
+    $("#scores").show();
+    $("#teams").hide();
+    $("#chat").hide();
+    $('#contact').hide();
+
+  });
+
+    $(".teams").click(function () {
+      
+    $("#title").empty();
+    $("#title").append("TEAMS");
+
+    $("#mainBackground").hide();
+    $("#news").hide();
+    $("#schedule").hide();
+    $("#scores").hide();
+    $("#teams").show();
+    $("#chat").hide();
+    $('#contact').hide();
+
+  });
+  
+    $(".chat").click(function () {
+      
+    $("#title").empty();
+    $("#title").append("CHAT");
+
+    $("#mainBackground").hide();
+    $("#news").hide();
+    $("#schedule").hide();
+    $("#scores").hide();
+    $("#teams").hide();
+    $("#chat").show();
+    $('#contact').hide();
+
+  });
+
+  $(".contact").click(function () {
+    
+    $("#title").empty();
+    $("#title").append("CONTACT");
+
+    $("#mainBackground").hide();
+    $("#news").hide();
+    $("#schedule").hide();
+    $("#scores").hide();
+    $("#teams").hide();
+    $("#chat").hide();
+    $('#contact').show();
+
+  });
+
+
+  //  $("#location").empty();
+
 });
